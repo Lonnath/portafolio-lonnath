559c1f6980a372d597dddbc03eb2766fbdbc9ae2
Lonnath <lonnath.dev@gmail.com>
2021-09-03 22:24:10 -0500

    MOD: se modifica configuracion de carpeta static.

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -117,9 +117,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
-STATICFILES_DIRS = (STATIC_ROOT, )
+if not DEBUG:
+    STATIC_ROOT = ''
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
