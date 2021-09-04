6257a5e8f73bb4dba4074b9eaae35f481550247e
Lonnath <lonnath.dev@gmail.com>
2021-09-04 00:34:53 -0500

    MOD: se modifica configuracion del servidor y librerias.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn portafolio-lonnath.wsgi
--- portafolio/settings.py
+++ portafolio/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -113,17 +113,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static_cdn', 'media_root')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/staticfiles/'
-if not DEBUG:
-    STATIC_ROOT = ''
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles/'),
-]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
