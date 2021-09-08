624ddcaa339c7428183861bd65718d230e97b80c
Lonnath <lonnath.dev@gmail.com>
2021-09-07 22:55:01 -0500

    MODIFICACIONES: 	- Se modifica la vista de administrador de proyectos.

--- proyectos/views.py
+++ proyectos/views.py
@@ -6,10 +6,16 @@ def principal(request):
 def registro_proyectos(request):
     if request.method == 'POST':
         loginTmp = validar_usuario(request.POST)
-        for fila in loginTmp.fields.values():
-            print(fila)
-        #if loginTmp.fields['usuario'] == 'lonnath' and loginTmp.fields['password'] == '1234':
-        formulario = Registro_proyecto()
-        return render(request, 'form_proyectos.html', {'formulario' : formulario})
+        if loginTmp.is_valid():
+            informacion = loginTmp.cleaned_data
+            if informacion['usuario'] == 'lonnath' and informacion['password'] == 'Vagato1697':
+                formulario = Registro_proyecto()
+                return render(request, 'form_proyectos.html', {'formulario' : formulario})
+            acces = 'Usuario o Contraseña Incorrectos'
+            acc_sty = 'badge bg-danger'
+    else:
+        acces = ''
+        acc_sty = ''
     login = validar_usuario()
-    return render(request, 'admin_proyectos.html', {'login' : login})
+    
+    return render(request, 'admin_proyectos.html', {'login' : login, 'acceso' : acces, 'acceso_estilo' : acc_sty})
